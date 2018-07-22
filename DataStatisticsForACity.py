a768f48aefd464f3f2715c3ffd60451997f44f9f
yizhouper <yipersevere@outlook.com>
2018-07-23 01:25:33 +0300

    continue cleaning

---
+++ DataStatisticsForACity.py
@@ -0,0 +1,181 @@
+import os
+import sys
+from pyspark.sql import SQLContext, SparkSession
+from pyspark.sql import Row
+
+import argparse
+import json
+
+
+def getArgsFromCommand():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", help="inputfilename.json")
+    parser.add_argument("-o", help="outputfilename.txt")
+    args = parser.parse_args()
+    try:
+        inputfile = args.i
+        outputfile = args.o
+        return inputfile, outputfile
+    except args as identifier:
+        print("input parameter error, input like: python DataStatistics.py -i [inputfilename.json] -o [outputfilename.txt]")
+        return
+
+def DataStatisticsBookingForACity():
+
+    spark = SparkSession.builder.appName("statistics for hotels reviews in Booking").config(
+        "spark.some.config.option", "booking").getOrCreate()
+    inputfile, outputfile = getArgsFromCommand()
+    data = spark.read.json(inputfile)
+    data = data.drop_duplicates()  # try to remove duplicate reviews
+
+    statistics = dict()
+    statistics["totalReviews"] = data.select(data.url).distinct().count()
+    statistics["totalHotels"] = data.select(data.hotelUrl).distinct().count()
+    statistics["avgReviewNumAHotel"] = statistics["totalReviews"] / statistics["totalHotels"]
+    statistics["0 stars"] = 0
+    statistics["1 stars"] = 0
+    statistics["2 stars"] = 0
+    statistics["3 stars"] = 0
+    statistics["4 stars"] = 0
+    statistics["5 stars"] = 0
+    statistics["null stars"] = 0
+    statistics["eachHotelDetail"] = list()
+    statistics["minReviewNumAHotel"] = statistics["totalReviews"]
+    statistics["maxReviewNumAHotel"] = 0
+
+    nullScoreNumAHotel = 0
+    nullScoreNum = 0
+    totalScoreAHotel = 0
+    totalScore = 0
+    # get statistics for each single hotel
+    for row in data.groupby(data.hotelUrl).count().collect():
+        oneHotel = dict()
+        oneHotel["reviewsNum"] = row["count"]
+        #     print(oneHotel["reviewsNum"])
+        if oneHotel["reviewsNum"] > statistics["maxReviewNumAHotel"]:
+            statistics["maxReviewNumAHotel"] = oneHotel["reviewsNum"]
+        if oneHotel["reviewsNum"] < statistics["minReviewNumAHotel"]:
+            statistics["minReviewNumAHotel"] = oneHotel["reviewsNum"]
+        oneHotel["hotelUrl"] = row["hotelUrl"]
+        try:
+            oneHotel["hotelStars"] = data.filter(data.hotelUrl == row["hotelUrl"]).first()["hotelStars"]
+        except:
+            oneHotel["hotelStars"] = None
+        oneHotel["0 stars"] = 0
+        oneHotel["1 stars"] = 0
+        oneHotel["2 stars"] = 0
+        oneHotel["3 stars"] = 0
+        oneHotel["4 stars"] = 0
+        oneHotel["5 stars"] = 0
+
+        for record in data.filter(data.hotelUrl == row["hotelUrl"]).collect():
+            if record["score"] == None:
+                nullScoreNumAHotel += 1
+                nullScoreNum += 1
+            else:
+                #             remove \n newline and convert string into float
+                try:
+                    totalScore += float(record["score"])
+                    totalScoreAHotel += float(record["score"])
+                except:
+                    nullScoreNumAHotel += 1
+                    nullScoreNum += 1
+                    pass
+
+                oneHotel[str(round(float(record["score"]) / 10 * 5)) + " stars"] += 1
+                statistics[str(round(float(record["score"]) / 10 * 5)) + " stars"] += 1
+        if (oneHotel["reviewsNum"] - nullScoreNumAHotel) == 0:
+            oneHotel["avgScore"] = None  # means all reviews aren't gaven scores.
+        else:
+            oneHotel["avgScore"] = totalScoreAHotel / (oneHotel["reviewsNum"] - nullScoreNumAHotel)
+        nullScoreNumAHotel = 0
+        statistics["eachHotelDetail"].append(oneHotel)
+    statistics["null stars"] = nullScoreNum
+    statistics["avgScore"] = totalScore / (statistics["totalReviews"] - nullScoreNum)
+    with open(outputfile, "w") as f:
+        json.dump(statistics, f)
+        f.close()
+
+
+def DataStatisticsTripadvisorForACity():
+    spark = SparkSession.builder.appName("hotel reviews statistics").config("spark.some.config.option",
+                                                                            "tripadvisor").getOrCreate()
+    inputfile, outputfile = getArgsFromCommand()
+    data = spark.read.json(inputfile)
+    data = data.drop_duplicates()  # try to remove duplicate reviews
+
+    statistics = dict()
+
+    statistics["totalReviews"] = data.select("url").distinct().count()
+    statistics["maxReviewNumAHotel"] = 0
+    statistics["minReviewNumAHotel"] = statistics["totalReviews"]
+
+    statistics["eachHotelDetail"] = list()
+
+    hotelsList = data.groupBy(data.hotelUrl).count().collect()
+    statistics["totalHotels"] = len(hotelsList)
+    statistics["avgReviewNumAHotel"] = statistics["totalReviews"] / len(hotelsList)
+
+    totalScore = 0
+    totalScoreForOneHotel = 0
+
+    nullScoreReviews = 0  # store the number which don't give score for all hotels reviews in the city
+
+    # calculate statistics for each hotel in this city
+    for row in hotelsList:
+        hotelDetails = dict()
+        hotelDetails["hotelUrl"] = row["hotelUrl"]
+
+        oneHotelData = data.filter(data.hotelUrl == row["hotelUrl"])
+
+        try:
+            #         hotelStars means hotel class for this hotel, not the review score.
+            hotelDetails["hotelStars"] = data.filter(data.hotelUrl == row["hotelUrl"]).first()["hotelStars"]
+        except:
+            hotelDetails["hotelStars"] = None
+
+        try:
+            hotelDetails["reviewsNum"] = oneHotelData.count()
+            if hotelDetails["reviewsNum"] > statistics["maxReviewNumAHotel"]:
+                statistics["maxReviewNumAHotel"] = hotelDetails["reviewsNum"]
+            if hotelDetails["reviewsNum"] < statistics["minReviewNumAHotel"]:
+                statistics["minReviewNumAHotel"] = hotelDetails["reviewsNum"]
+        except:
+            pass
+
+        nullScoreReviewsAHotel = 0
+        hotelDetails["avgScore"] = 0
+        try:
+            for scoreRow in oneHotelData.groupBy(oneHotelData.score).count().collect():
+                #         if the user doesn't give any score, then
+                if scoreRow["score"] == None:
+                    k = "null score"
+                    nullScoreReviews += 1
+                    nullScoreReviewsAHotel += 1
+                else:
+                    k = str(scoreRow["score"]) + " score"
+                    totalScoreForOneHotel += float(scoreRow["score"]) * float(scoreRow["count"])
+                hotelDetails[k] = scoreRow["count"]
+        except:
+            pass
+        # add total score that each hotel gets
+        totalScore += totalScoreForOneHotel
+        try:
+            hotelDetails["avgScore"] = totalScoreForOneHotel / (hotelDetails["reviewsNum"] - nullScoreReviewsAHotel)
+        except:
+            pass
+        totalScoreForOneHotel = 0
+        statistics["eachHotelDetail"].append(hotelDetails)
+
+    statistics["avgScore"] = totalScore / (statistics["totalReviews"] - nullScoreReviews)
+
+    # classify all reviews regardless hotels into 6 categories, 0, 1, 2, 3, 4, 5 scores
+    for scoreCityRow in data.groupBy(data.score).count().collect():
+        if scoreCityRow["score"] == None:
+            kCity = "null score"
+        else:
+            kCity = str(scoreCityRow["score"]) + " score"
+        statistics[kCity] = scoreCityRow["count"]
+    with open(outputfile, "w") as f:
+        json.dump(statistics, f)
+        f.close()
--- cleanRawReviews.py
+++ cleanRawReviews.py
@@ -5,40 +5,6 @@ import os
 from overview_data_helper import label_sentiment_category
 
 
-# def getCollection(colletName = ""):
-#     '''
-#     return pandas dataframe.
-#     '''
-#     cursor = db[colletName].find({})
-#     df = pd.DataFrame(list(cursor))
-#     return df
-#
-#
-# # connect to the db
-# client = MongoClient()
-# db = client.hotelreviews
-#
-# # take Barcelona city hotel reviews as example
-# cityName = "barcelonaTripadvisor"
-# df = getCollection(colletName = cityName)
-#
-#
-# # testCollNames = testCollTripadvisorNames + testCollBookingNames
-#
-# newDatabaseName = "sentimentAnalysis"
-# newDB = client[newDatabaseName]
-# # sentAnalysisList = list()
-# df.dropna(axis=0, how="any", inplace=True)
-# df.drop_duplicates(inplace=True)
-# # df.dropna(axis=0, how="any", inplace=True)
-# newDB.create_collection("barcelonaTripadvisor")
-# odo(df, newDB["barcelonaTripadvisor"])
-#
-#
-#
-#
-# print("finished")
-
 def get_files_name(folder_path):
     arr = os.listdir(folder_path)
     return arr
@@ -74,6 +40,92 @@ def train_test_split(df, test_percent=0.2):
 
     train_df, test_df = df.iloc[:train_size, :], df.iloc[train_size:, :]
     return train_df, test_df
+
+
+def change_mongo_collection_name(db=None.test):
+    '''
+    changing mongo collection name,
+    :param db: mongoDB database, default "test" database
+    :return:
+    '''
+    # 35 cities name
+    citiesName = ["Amsterdam", "Athens", "Barcelona", "Berlin", "Helsinki",
+                  "Paris", "Stockholm", "London", "Manchester", "Roma",
+                  "Prague", "Edinburgh", "Vienna", "Lisbon", "Budapest",
+                  "Madrid", "Warsaw", "Brussels", "Dublin", "Munich",
+                  "Riga", "hamburg","Vilnius", "Tallinn", "Reykjavik",
+                  "Frankfurt", "Zurich", "liverpool", "Minsk", "Oslo",
+                  "Kiev", "Bucharest", "Sofia", "Cologne", "Kharkiv"]
+
+    collNames = db.collection_names()
+    for i in collNames:
+        subNameList = i.split("_")
+        if len(subNameList) > 1:
+            if subNameList[1] in ["tripadvisor", "Tripadvisor"]:
+                newName = subNameList[0].lower() + "Tripadvisor"
+            if subNameList[1] in ["Booking", "booking"]:
+                newName = subNameList[0].lower() + "Booking"
+            else:#                 using original nameri
+                newName = "_".join(subNameList)
+            db[i].rename(newName)
+        else:
+            pass
+    return db.collection_names()
+
+
+def generate_train_test_data():
+
+    # file1 = "amsterdamTripavisor.csv"
+    # file2 = "/home/yi/sentimentAnalysis/data/athensTripavisor.csv"
+    # file3 = "/home/yi/sentimentAnalysis/data/barcelonaTripadvisor.csv"
+    # file4 = "/home/yi/sentimentAnalysis/data/dublinTripavisor.csv"
+    #
+    #
+    # df1 = pd.read_csv(file1)
+    # df2 = pd.read_csv(file2)
+    # df3 = pd.read_csv(file3)
+    # df4 = pd.read_csv(file3)
+    #
+    # df = pd.concat([df1, df2, df3, df4], axis=0)
+    # df.reset_index(drop=True)
+    # assert df.shape[0] == (df1.shape[0] + df2.shape[0] + df3.shape[0] + df4.shape[0])
+    # df.to_csv("raw_tripadvisor_data.csv")
+    #
+    #
+    # test_percent = 0.2
+    # train_size = int(df.shape[0] * (1 - test_percent))
+    #
+    # train_df, test_df = df.iloc[:train_size, :], df.iloc[train_size:, :]
+    # train_df.to_csv("raw_tripadvisor_data_train.csv")
+    # test_df.to_csv("raw_tripadvisor_data_test.csv")
+    #
+    #
+    # # reserve review and sentiment two column
+    # clean_train_df = pd.DataFrame()
+    # clean_train_df["review"] = train_df["review"]
+    # clean_train_df['sentiment'] = train_df.apply(lambda row: label_sentiment_category(row), axis=1)
+    #
+    # clean_test_df = pd.DataFrame()
+    # clean_test_df["review"] = test_df["review"]
+    # clean_test_df['sentiment'] = test_df.apply(lambda row: label_sentiment_category(row), axis=1)
+    #
+    # print("only sentiment and reviews")
+    #
+    #
+    # clean_train_df.to_csv("./csv/train_tripadvisor_5cities.csv")
+    # clean_test_df.to_csv("./csv/test_tripadvisor_5cities.csv")
+    #
+    file_train = "/home/yi/sentimentAnalysis/data/train_test_split/tripadvisor/train_tripadvisor.csv"
+    file_test = "/home/yi/sentimentAnalysis/data/train_test_split/tripadvisor/test_tripadvisor.csv"
+
+    five_score_train_df = pd.read_csv(file_train, index_col=0)
+    five_score_test_df = pd.read_csv(file_test, index_col=0)
+
+    names = ["5_score_test.csv", "5_score_train.csv"]
+
+    for name, df in zip(names, [five_score_test_df, five_score_train_df]):
+        df.drop(["_id", "date", "hotelLocation", "hotelName", "hotelStars", "hotelUrl", "title", "url", "userId"], axis=1, inplace=True)
+        df.to_csv("/home/yi/sentimentAnalysis/data/rev_sent_5_score_train_test/tripadvisor/"+name)
 
 
 
